cbde857c91261a9fe2e83f249e47dd1ef54e8dbb
Max Burri <burri@puzzle.ch>
2025-11-07 16:32:17 +0100

    feat: add organisation filter in react

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,24 +1,22 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react()],
   i18n: {
-      locales: ["en", "de", "fr", "it"],
-      defaultLocale: "en",
-      fallback: {
-          de: "en",
-          fr: "en",
-          it: "en"
-      },
-      routing: {
-          prefixDefaultLocale: false,
-          redirectToDefaultLocale: false,
-          fallbackType: "rewrite"
-      }
+    locales: ["en", "de", "fr", "it"],
+    defaultLocale: "en",
+    fallback: {
+      de: "en",
+      fr: "en",
+      it: "en",
+    },
+    routing: {
+      prefixDefaultLocale: false,
+      redirectToDefaultLocale: false,
+      fallbackType: "rewrite",
+    },
   },
-
-  integrations: [react()]
 });
