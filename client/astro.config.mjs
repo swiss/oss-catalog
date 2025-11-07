6f9ecc22b4af8c7a086db8eef88e49cca24df564
Max Burri <burri@puzzle.ch>
2025-11-07 16:30:16 +0100

    chore: add react

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,20 +1,24 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        locales: ["en", "de", "fr", "it"],
-        defaultLocale: "en",
-        fallback: {
-            de: "en",
-            fr: "en",
-            it: "en"
-        },
-        routing: {
-            prefixDefaultLocale: false,
-            redirectToDefaultLocale: false,
-            fallbackType: "rewrite"
-        }
-    }
+  i18n: {
+      locales: ["en", "de", "fr", "it"],
+      defaultLocale: "en",
+      fallback: {
+          de: "en",
+          fr: "en",
+          it: "en"
+      },
+      routing: {
+          prefixDefaultLocale: false,
+          redirectToDefaultLocale: false,
+          fallbackType: "rewrite"
+      }
+  },
+
+  integrations: [react()]
 });
