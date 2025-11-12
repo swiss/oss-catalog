23f7c904a43979637bfb975ef57a2262dc91464e
Claudia Asti <asti@puzzle.ch>
2025-11-12 11:29:34 +0100

    chore: add tailwind

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -3,22 +3,28 @@ import {defineConfig} from "astro/config";
 
 import react from "@astrojs/react";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        locales: ["en", "de", "fr", "it"],
-        defaultLocale: "en",
-        fallback: {
-            de: "en",
-            fr: "en",
-            it: "en"
-        },
-        routing: {
-            prefixDefaultLocale: false,
-            redirectToDefaultLocale: false,
-            fallbackType: "rewrite"
-        }
-    },
+  i18n: {
+      locales: ["en", "de", "fr", "it"],
+      defaultLocale: "en",
+      fallback: {
+          de: "en",
+          fr: "en",
+          it: "en"
+      },
+      routing: {
+          prefixDefaultLocale: false,
+          redirectToDefaultLocale: false,
+          fallbackType: "rewrite"
+      }
+  },
 
-    integrations: [react()]
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
