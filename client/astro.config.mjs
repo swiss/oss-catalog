6e9f9d78dcfc87950965ac258975b5788a78bbc0
Max Burri <burri@puzzle.ch>
2025-11-14 16:07:19 +0100

    chore: correct setup for github-pages deploy

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -3,18 +3,20 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        locales: ["en", "de", "fr", "it"],
-        defaultLocale: "en",
-        fallback: {
-            de: "en",
-            fr: "en",
-            it: "en"
-        },
-        routing: {
-            prefixDefaultLocale: false,
-            redirectToDefaultLocale: false,
-            fallbackType: "rewrite"
-        }
-    }
+  site: "https://swiss.github.io",
+  base: "/oss-catalog",
+  i18n: {
+    locales: ["en", "de", "fr", "it"],
+    defaultLocale: "en",
+    fallback: {
+      de: "en",
+      fr: "en",
+      it: "en",
+    },
+    routing: {
+      prefixDefaultLocale: false,
+      redirectToDefaultLocale: false,
+      fallbackType: "rewrite",
+    },
+  },
 });
