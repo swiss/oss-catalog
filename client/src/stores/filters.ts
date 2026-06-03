0d15d253375724136f8a9782b226cca98f1c4b76
Claudia Asti <asti@puzzle.ch>
2026-06-03 17:09:53 +0200

    fix: remove trailing slash from canton prefix

--- client/src/stores/filters.ts
+++ client/src/stores/filters.ts
@@ -2,7 +2,7 @@ import {atom} from "nanostores";
 
 export type OrganisationType = "all" | "bund" | "cantons";
 
-export const CANTON_URI_PREFIX = "https://ld.admin.ch/canton/";
+export const CANTON_URI_PREFIX = "https://ld.admin.ch/canton";
 
 export const selectedOrganisations = atom<string[]>([]);
 export const nameQuery = atom<string>("");
