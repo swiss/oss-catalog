527b308d901d5e7d3a2f588b5e06c2dabc0b252e
Claudia Asti <asti@puzzle.ch>
2026-06-04 15:41:59 +0200

    feat: add some flags (wip)

--- client/src/components/SoftwareCard.tsx
+++ client/src/components/SoftwareCard.tsx
@@ -4,6 +4,9 @@ import { RepositoryPlatformLogo } from "./RepositoryPlatformLogo.tsx";
 import { getYear } from "date-fns";
 import { type Lang, useTranslations } from "@/i18n/utils";
 import organisations from "@/data/organisations.json";
+import { getCantonFlag } from "@/utils/cantonFlags";
+import  schweizFlag  from "@/images/Wappen-Schweiz.svg";
+import { CANTON_URI_PREFIX } from "@/stores/filters";
 
 export function SoftwareCard({
   software,
@@ -23,22 +26,35 @@ export function SoftwareCard({
       .find((o) => o.id === uri)?.name?.[lang];
   };
 
+  const uri = software.publiccode.organisation?.uri;
+  const flag = uri?.startsWith(CANTON_URI_PREFIX)
+    ? getCantonFlag(uri)
+    : schweizFlag;
+  const flagLabel = uri?.startsWith(CANTON_URI_PREFIX)
+    ? toOrganisationName(uri)
+    : t('index.filter.option.bund');
+
   return (
     <div className="card card--default" has-icon="false">
       <div className="card__content">
         <div className="card__body">
-          <p className="meta-info">
-            {software.publiccode.releaseDate && (
-              <span className="meta-info__item">
-                {getYear(software.publiccode.releaseDate)}
-              </span>
+          <div className="flex items-center">
+            {flag && (
+              <img src={flag.src} alt={flagLabel} className="logo__flag mr-6" />
             )}
-            {software.publiccode.legal?.license && (
-              <span className="meta-info__item">
-                {software.publiccode.legal?.license}
-              </span>
-            )}
-          </p>
+            <p className="meta-info">
+              {software.publiccode.releaseDate && (
+                <span className="meta-info__item">
+                  {getYear(software.publiccode.releaseDate)}
+                </span>
+              )}
+              {software.publiccode.legal?.license && (
+                <span className="meta-info__item">
+                  {software.publiccode.legal?.license}
+                </span>
+              )}
+            </p>
+          </div>
           <div>
             <h2 className="card__title">{software.publiccode.name}</h2>
             <span className="meta-info meta-info__item">
