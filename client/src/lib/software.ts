2812db66608331b2a0046f146473d20f17a1d4b3
Max Burri <burri@puzzle.ch>
2026-08-14 14:27:52 +0200

    feat: show all relevant publiccode.yml properties on software detail page (#104)
    * wip: show all attrs
    
    * fix: convert github and gitlab blob urls

--- client/src/lib/software.ts
+++ client/src/lib/software.ts
@@ -26,32 +26,91 @@ export type Software = {
 export type PubliccodeYml = {
   publiccodeYmlVersion?: string;
   name: string;
+  applicationSuite?: string;
   url?: string;
   landingURL?: string;
-  releaseDate?: string;
+  isBasedOn?: string | string[];
   softwareVersion?: string;
+  releaseDate?: string;
+  logo?: string;
+  monochromeLogo?: string;
+  inputTypes?: string[];
+  outputTypes?: string[];
   platforms?: string[];
   categories?: string[];
+  usedBy?: string[];
+  fundedBy?: FundingSource[];
+  roadmap?: string;
   developmentStatus?: string;
   softwareType?: string;
-  legal: {
-    license?: string;
-  };
-  organisation: {
+  supports?: string[];
+  organisation?: {
     uri?: string;
     name?: string;
   };
-  description?: Record<
-    string,
-    {
-      localisedName?: string;
-      genericName?: string;
-      shortDescription?: string;
-      longDescription?: string;
-      documentation?: string;
-      apiDocumentation?: string;
-      features?: string[];
-      screenshots?: string[];
-    }
-  >;
+  intendedAudience?: {
+    countries?: string[];
+    unsupportedCountries?: string[];
+    scope?: string[];
+  };
+  description?: Record<string, LocalizedDescription>;
+  legal?: {
+    license?: string;
+    mainCopyrightOwner?: string;
+    repoOwner?: string;
+    authorsFile?: string;
+  };
+  maintenance?: {
+    type?: string;
+    contractors?: Contractor[];
+    contacts?: Contact[];
+  };
+  localisation?: {
+    localisationReady?: boolean;
+    availableLanguages?: string[];
+  };
+  dependsOn?: {
+    open?: Dependency[];
+    proprietary?: Dependency[];
+    hardware?: Dependency[];
+  };
+};
+
+export type LocalizedDescription = {
+  localisedName?: string;
+  genericName?: string;
+  shortDescription?: string;
+  longDescription?: string;
+  documentation?: string;
+  apiDocumentation?: string;
+  features?: string[];
+  screenshots?: string[];
+  videos?: string[];
+  awards?: string[];
+};
+
+export type FundingSource = {
+  name: string;
+  uri?: string;
+};
+
+export type Contact = {
+  name: string;
+  email?: string;
+  phone?: string;
+  affiliation?: string;
+};
+
+export type Contractor = {
+  name: string;
+  website?: string;
+  until?: string;
+};
+
+export type Dependency = {
+  name: string;
+  optional?: boolean;
+  version?: string;
+  versionMax?: string;
+  versionMin?: string;
 };
